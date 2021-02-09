c2d206315f84d5f38c0ace382583c6f03778a194
ilofv <ilofv@LAPTOP-NT630VL7>
2021-02-09 19:05:58 +0100

    Initial check-in of the Tidrapport code

---
+++ Tidrapport/src/test/java/com/tidrapport/TidrapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidrapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidrapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
